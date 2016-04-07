f8fe9fa87dd51619cc8d974bcccfe2ff2a307a1c
Eihror <igor_gmelo@hotmail.com>
2016-04-07 14:44:15 -0300

    Update Installer.php

--- install/Installer.php
+++ install/Installer.php
@@ -14,10 +14,8 @@ namespace Eihror\codeigniter;
 use Composer\Script\Event;
 
 class Installer {
-    //const DOCROOT = 'public';
+    const DOCROOT = 'public';
 
-    const DOCROOT = '/';
-    
     /**
      * Composer post install script
      *
@@ -26,15 +24,12 @@ class Installer {
     public static function postInstall(Event $event = null) {
         // Copy CodeIgniter files
         self::recursiveCopy('vendor/codeigniter/framework/application', 'application');
-        //mkdir(static::DOCROOT, 0755);
-        //copy('vendor/codeigniter/framework/index.php', static::DOCROOT . '/index.php');
-        //copy('dot.htaccess', static::DOCROOT . '/.htaccess');
-        copy('vendor/codeigniter/framework/index.php', static::DOCROOT . 'index.php');
-        copy('dot.htaccess', static::DOCROOT . '.htaccess');
+        mkdir(static::DOCROOT, 0755);
+        copy('vendor/codeigniter/framework/index.php', static::DOCROOT . '/index.php');
+        copy('dot.htaccess', static::DOCROOT . '/.htaccess');
 
         // Fix paths in index.php
-        //$file = static::DOCROOT . '/index.php';
-        $file = static::DOCROOT . 'index.php';
+        $file = static::DOCROOT . '/index.php';
         $contents = file_get_contents($file);
         $contents = str_replace(
                 '$system_path = \'system\';', '$system_path = \'../vendor/codeigniter/framework/system\';', $contents
